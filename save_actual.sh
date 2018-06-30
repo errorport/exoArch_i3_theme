da0dc64d2fa72cb918dfd470c6060c06355c9c99
Gergely Bencsik <becsegergely@gmail.com>
2018-06-30 15:22:56 +0200

    -

--- save_actual.sh
+++ save_actual.sh
@@ -7,6 +7,6 @@ cp ~/.xinitrc .
 cp ~/.Xresources .
 cp ~/.Xauthority .
 cp ~/.zshrc .
-cp ~/.oh-my-zsh/themes/exoArch.zsh-theme .
+#cp ~/.oh-my-zsh/themes/exoArch.zsh-theme .
 
 echo -e "\e[1;31mDone\e[0m"
